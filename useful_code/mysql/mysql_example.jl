43395636a34d6170ade571c5d3607f6a177349c3
Dilum Aluthge <dilum@aluthge.com>
2021-03-21 18:55:59 -0400

    Add MySQL code (#1)

---
+++ useful_code/mysql/mysql_example.jl
@@ -0,0 +1,13 @@
+using DataFrames
+
+host = "pursamydbcit.services.brown.edu"
+user = "daluthge"
+
+conn = open_mysql_connection(; host, user) # you will be prompted to enter your password
+
+DataFrame(mysql_execute(conn, "show databases;"))
+mysql_execute(conn, "use mimic_demo;");
+DataFrame(mysql_execute(conn, "show tables;"))
+df = DataFrame(mysql_execute(conn, "select * from PATIENTS;"))
+
+close_mysql_connection!(conn) # make sure to close the connection when you are done
